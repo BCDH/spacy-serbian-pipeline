dfd570780441f410116928f3b25bc219fe525af8
Toma Tasovac <ttasovac@humanistika.org>
2021-04-13 14:49:49 +0200

    lem

--- srp/lemmatizer.py
+++ srp/lemmatizer.py
@@ -15,11 +15,12 @@ class SerbianLemmatizer():
         self.lemma_lookup =  srsly.read_json('srp_lookups_data/srp_lemma_lookup.json')
         self.non_declinables = ["PUNCT", "ADP", "CCONJ", "SCONJ", "ADV"]
         #lookup_table = self.lookups.get_table("lemma_lookup", {})
+        # the problem is that Spacy tags Еј-Би-Сија as PUNCT!  
 
     def __call__(self, doc: Doc) -> Doc:
         for t in doc:
             if t.pos_ in self.non_declinables:
-                t.lemma_ = t.text
+                t.lemma_ = t.text.lower() # for isntance, if a sentence starts with a preposition
             else:
                 t.lemma_ = self.lemmatize(t.text, t.norm_, t.pos_, self.lemma_lookup, self.lemma_rules)
         return doc
@@ -41,8 +42,6 @@ class SerbianLemmatizer():
                 return lemma[0]
             elif isinstance(lemma, dict):
                 # dict = two or more key:string or key:list items
-                # it's enough to check the first value because
-                # it's either or, you can't combine strings and lists
                 # "коме": {
                 #   "PRON": [
                 #    "ко",
@@ -57,44 +56,67 @@ class SerbianLemmatizer():
                 #     "NOUN": "Проданов",
                 #     "ADJ": "Проданов"
                 #  },
-                if isinstance(next(iter(lemma.values())), str):
-                    #return "string"
-                    #temp hack b/c of data
-                    if pos == "AUX":
-                        return lemma["VERB"] or "a"
-                    elif (pos == "SCONJ" or pos == "CCONJ"):
-                        return lemma["CONJ"] or "b"
-                    elif (pos == "PROPN"):
-                        return lemma["NOUN"] or "c"
-                    elif (pos == "DET"):
-                        return lemma["PRON"] or "d"
+                #  "мора": {
+                #     "NOUN": [
+                #         "мора",
+                #         "море"
+                #     ],
+                #     "VERB": "морати"
+                # },
+
+                if pos in lemma.keys():
+                    if isinstance(lemma[pos], str):
+                        return lemma[pos]
+                    elif isinstance(lemma[pos], list):
+                        return lemma[pos][1] +" !l" # this is random; we would need more info (gender etc. to resolve lists! except for бирократ/а which we may need to turn into one form in json data )
                     else:
-                        return lemma.get(pos, "empty")
-
-
-                    ## theoretically we could have a pos in the datafile that's NOT matched by the statistical predictionn. what then?
+                        return "nodict" #at the moment ther are no dicts at this level; we may have them if we decide to further distinguish lists by adding morphosyntactic tagsх
                 else:
-                    return lemma.get(pos)[1] or "f"
-
-                # if pos in lemma:
-                #     return "checkifstr" # lemma[pos] can also be a list!!s
-                # #     #return lemma[pos]
-                # else:
-                #     return "fixthis"
-
-
+                    if pos == "AUX":
+                        if isinstance(lemma["VERB"], str):
+                            return lemma["VERB"]
+                        elif isinstance(lemma["VERB"], list):
+                            return lemma["VERB"][1] +" !l" #this is random
+                        else:
+                            return "nodictAtThisLevel AUX"
+                    elif (pos == "SCONJ" or pos == "CCONJ"):
+                        if isinstance(lemma["CONJ"], str):
+                            return lemma["CONK"]
+                        elif isinstance(lemma["CONK"], list):
+                            return lemma["CONJ"][1] +" !l"#this is random
+                        else:
+                            return "nodictAtThisLevel CONJ"
+                    elif pos == "PROPN":
+                        if isinstance(lemma["NOUN"], str):
+                            return lemma["NOUN"]
+                        elif isinstance(lemma["NOUN"], list):
+                            return lemma["NOUN"][1] +" !l"#this is random
+                        else:
+                            return "nodictAtThisLevel PROPN"
+                    elif pos == "DET":
+                        if isinstance(lemma["PRON"], str):
+                            return lemma["PRON"]
+                        elif isinstance(lemma["PRON"], list):
+                            return lemma["PRON"][1] +" !l"#this is random
+                        else:
+                            return "nodictAtThisLevel PRON"
+                    else:
+                        # here we have tokens that exist in the lookup tables
+                        # but not under the pos that spacy assigned to it. for instance if прилично was tagged as a NOUN but it's as ADJ and ADV in the lookups
+                        return token + " !mism"
             else:
+                # neither string, list or dict, which is impossilbe
+                # will get rid of this
                 return type(lemma).__name__ +":"+ token
-                #token = "!" + token
         else:
             #check normalized versions
             lemma = lookup.get(norm, None)
             if isinstance(lemma, str):
                 return lemma
             elif isinstance(token, dict):
-                return ""
+                return "!normdict"
                 #token = list(token.values())[0]
             else:
-                return "!" + token
+                return ""
+                #return "!" + token
                 #token = "!" + token
-        return token
